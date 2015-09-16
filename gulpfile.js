2aea605220726ca4c3a004fdecebee702ff5ecd5
g8up <g8up@qq.com>
2015-09-16 21:16:36 +0800

    add gulp-header

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var header = require('gulp-header');
+
+var pkg = require('./package.json');
+var banner = ['/**', ' * <%= pkg.name %> - <%= pkg.description %>', ' * @version v<%= pkg.version %>', ' * @author <%= pkg.author %>', ' * @link <%= pkg.homepage %>', ' */', ''].join('\n');
 
 gulp.task('script', function () {
 	gulp.src('qrcode.js')
 		.pipe(uglify())
+		.pipe(header(banner, {
+			pkg: pkg
+		}))
 		.pipe(rename({
 			extname: '.min.js'
 		}))
